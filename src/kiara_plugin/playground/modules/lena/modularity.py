46894a016c928aa205b295fe32a3c22e6adb1646
Lena_Jaskov <jaskov@posteo.de>
2023-03-21 11:42:11 +0100

    add modularity module

---
+++ src/kiara_plugin/playground/modules/lena/modularity.py
@@ -0,0 +1,59 @@
+from kiara.api import KiaraModule, ValueMapSchema
+import networkx as nx
+from networkx.algorithms import community
+from operator import itemgetter
+
+from kiara_plugin.network_analysis.models import NetworkData
+
+KIARA_METADATA = {
+    "authors": [
+        {"name": "Lena Jaskov", "email": "helena.jaskov@uni.lu"},
+    ],
+    "description": "Kiara modules for: network_analysis",
+}
+
+class ModularityCommunity(KiaraModule):
+    """Calculate modularity for each node and attach to node list as attribute.
+    """
+    
+    _module_type_name = 'compute.modularity_group'
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        result = {
+            "network_data": {
+                "type": "network_data",
+                "doc": "The network data to analyze.",
+            }
+        }
+        return result
+    
+    def create_outputs_schema(self):
+        return {
+            "modularity_network": {
+                "type": "network_data",
+                "doc": "Updated network data with modularity group assigned as a node attribute."
+            }
+        }
+    
+    def process(self, inputs, outputs):
+        edges = inputs.get_value_obj('network_data')
+
+        network_data: NetworkData = edges.data
+
+        G = network_data.as_networkx_graph(nx.Graph)
+
+        communities = community.greedy_modularity_communities(G)
+
+        modularity_dict = {} 
+        for i,c in enumerate(communities): # Loop through the list of communities, keeping track of the number for the community
+            for name in c: # Loop through each person in a community
+                modularity_dict[name] = i # Create an entry in the dictionary for the person, where the value is which group they belong to.
+
+        nx.set_node_attributes(G, modularity_dict, 'modularity')
+
+        attribute_network = NetworkData.create_from_networkx_graph(G)
+        
+        outputs.set_values(modularity_network=attribute_network)
